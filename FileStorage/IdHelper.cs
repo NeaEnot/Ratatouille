4218062f8f302529d63f8b7e7be9a97f96db9ef8
NeaEnot <oleg.kozloss@yandex.ru>
2022-03-06 10:48:48 +0400

    IdHelper

---
+++ FileStorage/IdHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Ratatouille.FileStorage
+{
+    internal static class IdHelper
+    {
+        private static string alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        internal static string GetId()
+        {
+            string answer = "";
+            int i = 2;
+            Random random = new Random();
+
+            while (true)
+            {
+                StringBuilder builder = new StringBuilder();
+
+                alphabet
+                    .ToArray()
+                    .OrderBy(e => Guid.NewGuid())
+                    .Take(random.Next(1, i))
+                    .ToList().ForEach(e => builder.Append(e));
+
+                answer = builder.ToString();
+
+                if (Context.Instanse.Recipes.Count(req => req.Id == answer) == 0)
+                    break;
+
+                i++;
+            }
+
+            return answer;
+        }
+    }
+}
