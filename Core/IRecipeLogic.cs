0e33dc642d0473d203059b1682bd9c0d965c2005
NeaEnot <oleg.kozloss@yandex.ru>
2022-03-06 10:24:06 +0400

    Интерфейс логики для рецепта

---
+++ Core/IRecipeLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Ratatouille.Core
+{
+    /// <include file='Docs.xml' path='docs/members[@name="IRecipeLogic"]/IRecipeLogic/*'/>
+    public interface IRecipeLogic
+    {
+        /// <include file='Docs.xml' path='docs/members[@name="IRecipeLogic"]/Create/*'/>
+        public void Create(Recipe model);
+
+        /// <include file='Docs.xml' path='docs/members[@name="IRecipeLogic"]/Read/*'/>
+        public Recipe Read(string id);
+
+        /// <include file='Docs.xml' path='docs/members[@name="IRecipeLogic"]/Find/*'/>
+        public List<Recipe> Find(string query);
+
+        /// <include file='Docs.xml' path='docs/members[@name="IRecipeLogic"]/Update/*'/>
+        public void Update(Recipe model);
+
+        /// <include file='Docs.xml' path='docs/members[@name="IRecipeLogic"]/Delete/*'/>
+        public void Delete(string id);
+    }
+}
