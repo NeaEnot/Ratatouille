685056cceb99688fbab52c9e6941898e0c5d8e7d
NeaEnot <oleg.kozloss@yandex.ru>
2022-08-02 03:40:45 +0400

    При просмотре рецепта скрывать пустые поля

--- GUI/Windows/RecipeViewWindow.xaml.cs
+++ GUI/Windows/RecipeViewWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Ratatouille.Core;
 using Ratatouille.GUI.Pages;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Documents;
@@ -18,6 +19,27 @@ namespace Ratatouille.GUI.Windows
             frameTools.Content = new ImagedFieldPage(recipe.Tools, recipe.Images);
             frameInstruction.Content = new ImagedFieldPage(recipe.Instruction, recipe.Images);
             frameNotes.Content = new ImagedFieldPage(recipe.Notes, recipe.Images);
+
+            Dictionary<string, Field> fields =
+                new Dictionary<string, Field>
+                {
+                    { recipe.Tags, new Field { Title = tblTagsTitle, Content = tblTags } },
+                    { recipe.ApproxTime, new Field { Title = tblApproxTimeTitle, Content = tblApproxTime } },
+                    { recipe.Ingredients, new Field { Title = tblIngridientsTitile, Content = frameIngridients } },
+                    { recipe.Tools, new Field { Title = tblToolsTitile, Content = frameTools } },
+                    { recipe.Instruction, new Field { Title = tblInstructionTitile, Content = frameInstruction } },
+                    { recipe.Notes, new Field { Title = tblNotesTitile, Content = frameNotes } },
+                    { recipe.Links.Count > 0 ? recipe.Links[0] : "", new Field { Title = tblLinksTitle, Content = lbLinks } }
+                };
+
+            foreach (string key in fields.Keys)
+            {
+                if (string.IsNullOrWhiteSpace(key))
+                {
+                    fields[key].Title.Visibility = Visibility.Hidden;
+                    fields[key].Content.Visibility = Visibility.Hidden;
+                }
+            }
         }
 
         private void Hyperlink_Click(object sender, RequestNavigateEventArgs e)
@@ -25,6 +47,12 @@ namespace Ratatouille.GUI.Windows
             string destinationurl = (sender as Hyperlink).NavigateUri.ToString();
             ProcessStartInfo sInfo = new ProcessStartInfo(destinationurl) { UseShellExecute = true, };
             Process.Start(sInfo);
+        }
+
+        private class Field
+        {
+            internal UIElement Title { get; set; }
+            internal UIElement Content { get; set; }
         }
     }
 }
