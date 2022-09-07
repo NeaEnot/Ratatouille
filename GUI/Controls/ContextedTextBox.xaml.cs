543f946a377e77fd4c3b7e49c57c7df4984b2999
NeaEnot <oleg.kozloss@yandex.ru>
2022-09-07 11:50:26 +0400

    Контрол, содержаший textbox с обновляющимся контекстным меню

---
+++ GUI/Controls/ContextedTextBox.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Ratatouille.GUI.Controls
+{
+    public partial class ContextedTextBox : UserControl
+    {
+        public string Text
+        {
+            get => tb.Text;
+            set => tb.Text = value;
+        }
+
+        public Action<List<string>> UpdateLinks;
+
+        public ContextedTextBox()
+        {
+            InitializeComponent();
+            UpdateLinks = UpdateCurrentLinks;
+        }
+
+        private void UpdateCurrentLinks(List<string> links)
+        {
+            miInsertFromCurrent.Items.Clear();
+
+            for (int i = 0; i < links.Count; i++)
+            {
+                MenuItem item = new MenuItem
+                {
+                    Header = $"{i}: {links[i]}",
+                    Command = new Command(e => { tb.Text.Insert(tb.CaretIndex, $"<img_{i};400x300>"); })
+                };
+
+                miInsertFromCurrent.Items.Add(item);
+            }
+        }
+
+        private class Command : ICommand
+        {
+            private Action<object> execute;
+            private Func<object, bool> canExecute;
+
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+
+            public Command(Action<object> execute, Func<object, bool> canExecute = null)
+            {
+                this.execute = execute;
+                this.canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return canExecute == null || canExecute(parameter);
+            }
+
+            public void Execute(object parameter)
+            {
+                execute(parameter);
+            }
+        }
+    }
+}
